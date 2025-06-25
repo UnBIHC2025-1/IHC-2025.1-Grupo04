8d67d9834179df75d4c9f3e1a08e5449e83a3962
Diogo-Barboza <diogorodriguesbb@gmail.com>
2025-06-25 03:18:03 -0300

    fix grafico

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# main.py
-
 import re
 from pathlib import Path
 
 def define_env(env):
     @env.macro
     def contar_checklist(caminho_md):
-        # Lê o conteúdo do markdown
         conteudo = Path(caminho_md).read_text(encoding='utf-8')
 
-        # Conta checkboxes marcadas e desmarcadas
         feitos = len(re.findall(r'- \[x\]', conteudo, flags=re.IGNORECASE))
         pendentes = len(re.findall(r'- \[ \]', conteudo))
 
